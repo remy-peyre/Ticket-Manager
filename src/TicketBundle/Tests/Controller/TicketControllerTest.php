66b1e76499586065fbbe5ca1ef98929290d9ba52
Skipcat <cecile.huguet@supinternet.fr>
2017-11-16 10:08:11 +0100

    added ticket controller test

---
+++ src/TicketBundle/Tests/Controller/TicketControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace TicketBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TicketControllerTest extends WebTestCase
+{
+    /*
+    public function testCompleteScenario()
+    {
+        // Create a new client to browse the application
+        $client = static::createClient();
+
+        // Go to the list view
+        $crawler = $client->request('GET', '/ticket/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /ticket/");
+
+        // Go to the show view
+        $crawler = $client->click($crawler->selectLink('show')->link());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code");
+    }
+
+    */
+}
